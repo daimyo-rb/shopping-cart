d3026e2ff05029da510542dd7d3aec3b9870c52b
daimyo-rb <daimyo@roshibros.com>
2025-07-17 20:25:32 -0500

    Create placeholders for shop items

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,12 +1,19 @@
 import styles from "./Shop.module.css";
 import { Link } from "react-router-dom";
+import ShopItem from "../ShopItem/ShopItem";
 
 function Shop() {
   return (
-    <div className={styles.container}>
-      <h1>Shop</h1>
-      <Link to="/">Home</Link>
-    </div>
+    <>
+      <h1>Items</h1>
+      <div className={styles.container}>
+        <ShopItem />
+        <ShopItem />
+        <ShopItem />
+        <ShopItem />
+        <ShopItem />
+      </div>
+    </>
   )
 }
 
---
+++ src/components/ShopItem/ShopItem.jsx
@@ -0,0 +1,11 @@
+import styles from "./ShopItem.module.css";
+
+function ShopItem() {
+  return (
+    <div className={styles.card}>
+      I'm a shop item
+    </div>
+  )
+}
+
+export default ShopItem
