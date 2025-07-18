e77dcf08ae567c50a200a4e9d0b1de17bd1d8bd1
daimyo-rb <daimyo@roshibros.com>
2025-07-17 19:44:51 -0500

    Create styling for app to wrap Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import styles from "./App.module.css";
 import { Link, Outlet } from "react-router-dom";
 import Header from './components/Header/Header';
 
@@ -5,7 +6,7 @@ function App() {
   return (
     <>
       <Header />
-      <div>
+      <div className={styles.container}>
         <Outlet />
       </div>
     </>
