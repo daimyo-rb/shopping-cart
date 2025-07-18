24eabe956ced839998b052ccf6bf021153cdb511
daimyo-rb <daimyo@roshibros.com>
2025-07-17 22:49:19 -0500

    Create basic home page layout

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,11 @@
 import styles from "./Home.module.css";
-import { Link } from "react-router-dom";
 
 function Home() {
+  const heroImg = 'https://images.unsplash.com/photo-1625214214702-e2d227cfa495?q=80&w=874&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
   return (
     <div className={styles.container}>
-      <h1>Home</h1>
-      <Link to="shop">Shop</Link>
+      <img className={styles.heroImg} src={heroImg}/>
+      <p className={styles.welcomeText}>Welcome to my store!</p>
     </div>
   )
 }
